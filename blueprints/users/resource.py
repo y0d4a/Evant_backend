6f3a1aac254df1efc59f87e1abb57c7584176fe5
RadenMahar <panji@alterra.id>
2019-09-11 21:03:51 +0700

    add user testing

--- blueprints/users/resource.py
+++ blueprints/users/resource.py
@@ -3,7 +3,6 @@ from flask import Blueprint
 from flask_restful import Resource, Api, reqparse, marshal, inputs
 from sqlalchemy import desc
 from .model import Users
-import requests
 
 from flask_jwt_extended import jwt_required, create_access_token, get_jwt_identity, get_jwt_claims
 
