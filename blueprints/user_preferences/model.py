18dd7513de2fa65c2419af35eefb5f5e84449eda
Syamsul <syamsul@alterra.id>
2019-09-16 16:10:58 +0700

    add confirmation field in user preference

--- blueprints/user_preferences/model.py
+++ blueprints/user_preferences/model.py
@@ -7,14 +7,17 @@ class UserPreferences(db.Model):
     user_id = db.Column(db.Integer, nullable = False, primary_key=True)
     event_id = db.Column(db.Integer, nullable = False, primary_key=True)
     preference = db.Column(db.String(50), nullable= False)
+    confirmation = db.Column(db.Integer, nullable=False)
 
     response_fields = {
         'user_id' : fields.Integer,
         'event_id' : fields.Integer,
-        'preference' : fields.String
+        'preference' : fields.String,
+        'confirmation':fields.Integer
     }
 
-    def __init__(self,user_id, event_id, preference):
+    def __init__(self,user_id, event_id, preference, confirmation=0):
         self.user_id = user_id
         self.event_id = event_id
         self.preference = preference
+        self.confirmation = confirmation
--- blueprints/user_preferences/resource.py
+++ blueprints/user_preferences/resource.py
@@ -2,6 +2,8 @@ import json
 from flask import Blueprint
 from flask_restful import Api, Resource, reqparse, marshal
 from .model import UserPreferences
+from blueprints.events.model import Events
+from blueprints.users.model import Users
 from flask_jwt_extended import get_jwt_identity, jwt_required
 from blueprints import db, app
 
@@ -79,8 +81,58 @@ class UserPreferencesResources (Resource) :
         app.logger.debug('DEBUG : %s', user_preferences)
         
         return preferences, 200, {'Content-Type': 'application/json'}
-    
+
+class ConfirmationResources(Resource):
+    @jwt_required
+    def get(self, event_id):
+        identity = get_jwt_identity()
+        user_id = identity['user_id']
+
+        current_event = Events.query.get(event_id)
+
+        '''get event confirmation status'''
+        confirmations = UserPreferences.query.filter_by(event_id=event_id).all()
+
+        result = []
+
+        '''convenient output'''
+        for confirmation in confirmations:
+            current_user = Users.query.get(confirmation.user_id)
+            temp = {
+                'confirmation': confirmation.confirmation,
+                'username':current_user.username,
+                'fullname':current_user.fullname,
+                'preference':confirmation.preference,
+                'user_id':current_user.user_id
+            }
+            result.append(temp)
+
+        return result, 200, {'Content-Type': 'application/json'}
+
+    @jwt_required
+    def put(self,event_id):
+
+        """
+        method to edit user preferences to certain event
+        """	
+        identity = get_jwt_identity()
+        user_id = identity['user_id']
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('confirmation', location='json', type=int, required=True)
+
+        args = parser.parse_args()
+
+        preference_query = UserPreferences.query.filter_by(event_id=event_id, user_id=user_id).first()
+
+        preference_query.confirmation = args['confirmation']
+        db.session.commit()
+
+        app.logger.debug('DEBUG : %s', preference_query)
+
+        return marshal(preference_query, UserPreferences.response_fields), 200, {'Content-Type': 'application/json'}
 
 
 
 api.add_resource(UserPreferencesResources, '', '/<event_id>')
+api.add_resource(ConfirmationResources, '/confirmations/<event_id>')
