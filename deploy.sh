6dc26384086b2e9f9a75b2ef3e2f675f6fb2993d
RadenMahar <panji@alterra.id>
2019-09-17 22:55:38 +0700

    add deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-sudo docker stop agatharach/backend-project:latest
-sudo docker rm agatharach/backend-project:latest
+sudo docker stop backend
+sudo docker rm backend
 sudo docker rmi agatharach/backend-project
 sudo docker run -d --name backend -p 5000:5000 agatharach/backend-project:latest
 
